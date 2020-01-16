f1c8451605edce248e972e88e8db13a1efbbe282
Pawel Buda <p-buda@users.noreply.github.com>
2020-01-15 20:37:18 -0600

    Split template and donut shop into separate folders.

---
+++ 01Unit/template/js/app.js
@@ -0,0 +1,11 @@
+/* 
+    app.js - JS code for the donut shop page.
+    Author:
+    Created: mm-dd-yyyy
+    Edited: mm-dd-yyyy
+    Notes:
+*/
+
+console.log('The app started.');
+
+window.alert('1-2-3, raspberry jam!');
