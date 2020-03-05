ca2fe61637721f8dd87fb8d8c4da50e6e7793a48
Pawel Buda <p-buda@users.noreply.github.com>
2020-03-04 20:33:50 -0600

    Replace quotes with double-quotes

--- 08Week/even_odd_checker/js/even_odd.js
+++ 08Week/even_odd_checker/js/even_odd.js
@@ -4,28 +4,28 @@
     Created: 10-21-2019
     Edited: 10-21-2019
 */
-'use strict;'
+"use strict;"
 
 $(document).ready(function() {
 
-    $('#check_button').click(function() {
-        var userNumber = $('#user_number').val();
+    $("#check_button").click(function() {
+        var userNumber = $("#user_number").val();
         userNumber = parseInt(userNumber);
-        if(userNumber === '' || isNaN(userNumber)) {
-            $('#user_number').next().text(' Enter a valid number');
+        if(isNaN(userNumber) || userNumber === "") {
+            $("#user_number").next().text(" Enter a valid number");
         } else {
             if(userNumber % 2 === 0) {
-                $('.answer').text(userNumber + ': this number is even.')
+                $(".answer").text(userNumber + ": this number is even.")
             }
             else {
-                $('.answer').text(userNumber + ': this number is odd.')
+                $(".answer").text(userNumber + ": this number is odd.")
             }
         }
     }); // End of button click()
 
     $("#user_number").click(function() {
         $("#user_number").next().text(" *");
-        $('#user_number').val("");
+        $("#user_number").val("");
     }); // End of input click
 
 }); // End of ready()
