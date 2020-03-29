9f3f5d2c139d2dfd5d9a0d02b50f577455da99e2
Pawel Buda <p-buda@users.noreply.github.com>
2020-03-29 17:37:07 -0500

    Implement jQuery datepicker

---
+++ 11Unit/js_formValidation/js/plugins.js
@@ -0,0 +1,14 @@
+/* 
+    plugins.js
+    Author: SCC
+    Created: 3-29-2020
+    Edited: 3-28-2020
+*/
+"use strict;"
+
+$(document).ready(function() {
+    $("#class_date").datepicker({
+        minDate: new Date(),
+        showButtonPanel: true
+    });
+});
